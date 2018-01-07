7174feaa7bce3cd242b02cdc0a0458edbe2959f3
jonatjano <jonathan.selle@laposte.net>
2018-01-07 21:45:57 +0100

    added some traduction for FRfr

--- src/server/config/language/FRfr.java
+++ src/server/config/language/FRfr.java
@@ -14,10 +14,13 @@ public class FRfr extends AbstractLanguage
 		/* Config file messages */
 		/************************/
 		ret.put("ConfigFileDoesNotExist", "Le fichier de config entré n'existe pas");
+		ret.put("ConfigFileCantBeRead", "Le fichier de config ne peut être lu, les valeurs par défaut seront utilisées");
 		ret.put("ConfigDoesNotExist", "Il n'y as pas de valeur pour la configuration suivante :");
+		ret.put("InvalidConfigValue", "La valeur n'est pas valide pour la configuration suivante :");
 		ret.put("CreatingConfigFile", "Il va maintenant être créé");
 		ret.put("FailedToCreateDefaultConfigFile", "Echec de la création du fichier de configuration veuillez réessayer");
 		ret.put("FileIsCreatedModifyBeforeRestarting", "Le fichier de configuration à été créer. Merci de l'adapter à vos besoin avant de redémarrer le serveur");
+		ret.put("IHMTypeNotValid", "Ce type d'interface n'est pas valide");
 
 		/**********************/
 		/*  General messages  */

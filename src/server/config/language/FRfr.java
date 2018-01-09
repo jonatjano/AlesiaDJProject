f41c3a33c5f1a432c81845cef64368ef91e48c0e
jonatjano <jonathan.selle@laposte.net>
2018-01-09 22:16:19 +0100

    added some translaation for french

--- src/server/config/language/FRfr.java
+++ src/server/config/language/FRfr.java
@@ -16,26 +16,36 @@ class FRfr extends AbstractLanguage
 	@Override
 	Map<String, String> createTranslationMap()
 	{
-		Map<String, String> ret = new HashMap<String, String>();
+		Map<String, String> tm = new HashMap<String, String>();
 
 		/************************/
 		/* Config file messages */
 		/************************/
-		ret.put("ConfigFileDoesNotExist", "Le fichier de config entré n'existe pas");
-		ret.put("ConfigFileCantBeRead", "Le fichier de config ne peut être lu, les valeurs par défaut seront utilisées");
-		ret.put("ConfigDoesNotExist", "Il n'y as pas de valeur pour la configuration suivante :");
-		ret.put("InvalidConfigValue", "La valeur n'est pas valide pour la configuration suivante :");
-		ret.put("CreatingConfigFile", "Il va maintenant être créé");
-		ret.put("FailedToCreateDefaultConfigFile", "Echec de la création du fichier de configuration veuillez réessayer");
-		ret.put("FileIsCreatedModifyBeforeRestarting", "Le fichier de configuration à été créer. Merci de l'adapter à vos besoin avant de redémarrer le serveur");
-		ret.put("IHMTypeNotValid", "Ce type d'interface n'est pas valide");
+		tm.put("ConfigFileDoesNotExist", "Le fichier de config entré n'existe pas");
+		tm.put("ConfigFileCantBeRead", "Le fichier de config ne peut être lu, les valeurs par défaut seront utilisées");
+		tm.put("ConfigDoesNotExist", "Il n'y as pas de valeur pour la configuration suivante : ");
+		tm.put("InvalidConfigValue", "La valeur n'est pas valide pour la configuration suivante : ");
+		tm.put("CreatingConfigFile", "Il va maintenant être créé");
+		tm.put("FailedToCreateDefaultConfigFile", "Echec de la création du fichier de configuration veuillez réessayer");
+		tm.put("FileIsCreatedModifyBeforeRestarting", "Le fichier de configuration à été créer. Merci de l'adapter à vos besoin avant de redémarrer le serveur");
+		tm.put("PrintingConfigs", "Affichage des configurations :");
+		tm.put("CreatingConfigFileIn", "Création du fichier de configurations à l'emplacement : ");
 
-		/**********************/
-		/*  General messages  */
-		/**********************/
-		ret.put("PleaseContactDevs", "Merci d'informer les développeurs pour que ce problème soit réglé au plus vite, merci d'inclure ce message d'erreur dans le message");
-		ret.put("ServerIsGoingDown", "Le server est en cours d'extinction, veuillez patienter");
+		/************************/
+		/*     IHM messages     */
+		/************************/
+		tm.put("IHMTypeNotValid", "Ce type d'interface n'est pas valide");
 
-		return ret;
+		/************************/
+		/*   General messages   */
+		/************************/
+		tm.put("PleaseContactDevs", "Merci d'informer les développeurs pour que ce problème soit réglé au plus vite, merci d'inclure ce message d'erreur dans le message ainsi que le contexte dans lequel l'erreur est apparue");
+		tm.put("ServerIsGoingDown", "Le server est en cours d'extinction, veuillez patienter");
+		tm.put("FailedToCloseSocket", "Echec lors de la fermeture du la Connection, si le port n'est pas libéré proprement peut être sera-t-il nécéssaire de rallumer la machine");
+		tm.put("UnknowError", "Une erreur inconnue est apparue, si le problème persiste après plusieurs essais, " + tm.get("PleaseContactDevs"));
+		tm.put("ERROR", "ERROR : ");
+		tm.put("INFO", "INFO : ");
+
+		return tm;
 	}
 }
